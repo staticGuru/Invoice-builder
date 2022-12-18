217ff5e8abe8e496aeaafa1c19a2f8e2e8a618b0
staticGuru <vigneshguru274@gmail.com>
2022-12-18 16:14:50 +0530

    add style in the header

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App d-flex flex-column align-items-center justify-content-center w-100">
         <Container>
-          <div className="d-flex flex-row text-center pt-2 pl-2">
+          <div className="d-flex flex-row text-center pt-2 pl-2 align-center">
           <img src={Logo} alt="logo" style={{width:40,height:40,marginRight:10}} />
             <h2 className="fw-bold" style={{ color: '#0b5ed7' }}>
               InvoiceDesk.in
